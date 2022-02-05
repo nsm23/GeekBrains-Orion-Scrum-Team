0170357765a542e5e57acf25ddd1e69bf5a65ae4
Maksym Tymoshchenko <maksym.tymoshchenko@outlook.com>
2022-02-05 17:06:18 +0200

    GOST-50 Post detail view, minor fixes

--- orion/orion/urls.py
+++ orion/orion/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf.urls.static import static
 from django.conf import settings
 
-print(settings.MEDIA_URL)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('posts/', include('posts.urls', namespace='posts'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- orion/posts/views.py
+++ orion/posts/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django import template
+from django.utils import timezone
+from django.views.generic.detail import DetailView
+from django.http import Http404
 
-# Create your views here.
+from posts.models import Post
+
+class PostDetailView(DetailView):
+
+    model = Post
+    template_name = 'posts/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        
+        context['page_title'] = 'Просмотр поста'
+        return context
