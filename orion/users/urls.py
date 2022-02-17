ab092e76586c7dcce443286c4b9cf94cfffaa293
Sergey <nsergeym@gmail.com>
2022-02-17 09:11:19 +0300

    GOST-9 Добавить возможность пользователю зарегистрироваться и автоизоваться

--- orion/users/urls.py
+++ orion/users/urls.py
@@ -2,9 +2,12 @@ from django.urls import path
 
 from . import views
 
-
 app_name = 'users'
 urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
+    path('logout/', views.logout, name='logout'),
+
     path('<int:pk>/edit/', views.UserUpdateView.as_view(), name='user_edit'),
     path('<int:pk>/', views.UserDetailView.as_view(), name='user_detail_pk'),
     path('<str:slug>/', views.UserDetailView.as_view(), name='user_detail_slug'),
