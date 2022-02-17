1a809249a394f71b5e5813a12fd57ea208ee8436
AEpolyakov <ae.polyakov@mail.ru>
2022-02-17 15:00:10 +0400

    GOST-60 страница создания поста

--- orion/posts/urls.py
+++ orion/posts/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import PostDetailView
-from django.conf import settings
+from .views import PostDetailView, PostCreateView
 
 
 app_name = 'posts'
 urlpatterns = [
+    path('create/', PostCreateView.as_view(), name='create-post'),
     path('<slug:slug>/', PostDetailView.as_view(), name='detail'),
 ]
--- orion/posts/views.py
+++ orion/posts/views.py
@@ -1,4 +1,10 @@
+from django.urls import reverse
 from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView
+from django.http import HttpResponseRedirect
+from django.template.defaultfilters import slugify
+from django.core.files.storage import FileSystemStorage
+
 from posts.models import Post
 
 
@@ -14,3 +20,25 @@ class PostDetailView(DetailView):
         context['comments_list'] = self.object.comments.filter(active=True, parent__isnull=True)
 
         return context
+
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'posts/post_form.html'
+    fields = ['title', 'brief_text', 'text', 'image', 'hub']
+
+    def form_valid(self, form):
+        self.object = form.save()
+        publish = 'publish' in self.request.POST
+        self.object.status = Post.ArticleStatus.ACTIVE if publish else Post.ArticleStatus.DRAFT
+        self.object.slug = slugify(self.object.title)
+        self.object.user = self.request.user
+        post_image = self.request.FILES['image']
+        fs = FileSystemStorage()
+        fs.save(post_image.name, post_image)
+
+        self.object.save()
+        if publish:
+            return HttpResponseRedirect(reverse('main'))
+        return HttpResponseRedirect(reverse('cabinet:user_profile',
+                                            kwargs={'pk': self.request.user.id, 'section': 'user_drafts'}))
