c252fa765c729c92febc39993eaac5843c8a2013
konstantin_paulkin_tgt <konstantin.paulkin@tgtdiagnostics.com>
2022-02-18 16:08:36 +0300

    GOST-58 | Fix date format for user update form

--- orion/orion/settings.py
+++ orion/orion/settings.py
@@ -93,16 +93,6 @@ WSGI_APPLICATION = 'orion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#             'ENGINE': 'django.db.backends.postgresql',
-#             'NAME': os.getenv('POSTGRES_NAME'),
-#             'USER': os.getenv('POSTGRES_USER'),
-#             'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
-#             'HOST': os.getenv('POSTGRES_HOST'),
-#             'PORT': os.getenv('POSTGRES_PORT'),
-#     }
-# }
 DATABASES = {
     'default': {
             'ENGINE': 'django.db.backends.postgresql',
