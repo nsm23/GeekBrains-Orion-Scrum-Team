93744cc0291b36ac5cc56e7f3287e5143f91443e
captainKoala <kostjan-pirat@rambler.ru>
2022-03-10 20:16:53 +0300

    GOST-102 | Update post list for moderation: set deactivate-class for both approve/decline buttons.

--- orion/orion/settings.py
+++ orion/orion/settings.py
@@ -114,12 +114,12 @@ DATABASES = {
             'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 # Password validation
