22187545e5dc585a4846714a143afe8a23fdab68
konstantin_paulkin_tgt <konstantin.paulkin@tgtdiagnostics.com>
2022-03-10 11:49:06 +0300

    GOST-87 | Update the moderation posts template.

--- orion/moderation/views.py
+++ orion/moderation/views.py
@@ -6,3 +6,5 @@ from posts.models import Post
 class PostModerationListView(ListView):
     model = Post
     template_name = 'moderation/posts_list.html'
+    context_object_name = 'posts'
+    queryset = Post.objects.filter(status=Post.ArticleStatus.MODERATION)
