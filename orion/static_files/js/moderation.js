ca6006d2e7423119d29bdfeb729aedb84115dfed
captainKoala <kostjan-pirat@rambler.ru>
2022-03-12 17:16:49 +0300

    GOST-102 | Refactor post approval script. Add link to moderation page into header. Minor fixes for styles.

--- orion/static_files/js/moderation.js
+++ orion/static_files/js/moderation.js
@@ -2,11 +2,20 @@
 
 
 const POST_APPROVE_URL = "/moderation/posts/approve/{{post_id}}/";
+const POST_DECLINE_URL = "/moderation/posts/decline/{{post_id}}/";
 
 
-const postApproveFetch = post_id => {
+const postModerationFetch = (post_id, action) => {
+    let url;
+    if (action === 'approve') {
+        url = POST_APPROVE_URL.replace('{{post_id}}', post_id);
+    } else if (action === 'decline')
+        url = POST_DECLINE_URL.replace('{{post_id}}', post_id);
+    else
+        return ;
+
     const csrftoken = getCookie('csrftoken');
-    const request = new Request(POST_APPROVE_URL.replace('{{post_id}}', post_id));
+    const request = new Request(url);
     const options = {
         method: "POST",
         mode: "same-origin",
@@ -16,33 +25,41 @@ const postApproveFetch = post_id => {
 }
 
 
+const moderationBtnClick = (event, action) => {
+    event.preventDefault();
+
+    let a = event.target.closest("a");
+    postModerationFetch(a.dataset.postId, action)
+        .then(response => {
+            if ("error" in response)
+                console.log(`Post ${action} error: ${response["error"]}`)
+            else {
+                let innerBtns = a.parentElement.querySelectorAll('a');
+                for (let inner_btn of innerBtns) {
+                    inner_btn.classList.remove('btn-outline-success');
+                    inner_btn.classList.remove('btn-outline-danger');
+                    inner_btn.classList.add('disabled');
+                    inner_btn.classList.add('btn-outline-secondary');
+                    inner_btn.blur();
+                }
+            }
+        })
+        .catch(
+            // ToDo: catch errors!
+        );
+}
+
+
 document.addEventListener('DOMContentLoaded', event => {
     let postApproveBtns = document.querySelectorAll('.post-approve');
+    let postDeclineBtns = document.querySelectorAll('.post-decline');
 
     for (let btn of postApproveBtns)
         btn.addEventListener("click", event => {
-            event.preventDefault();
-
-            let a = event.target.closest("a");
-            postApproveFetch(a.dataset.postId)
-                .then(response => {
-                    if ("error" in response)
-                        console.log("Post approving error: " + response["error"])
-                    else {
-                        let innerBtns = a.parentElement.querySelectorAll('a');
-
-                        console.log(innerBtns);
-                        for (let inner_btn of innerBtns) {
-                            inner_btn.classList.remove('btn-outline-success');
-                            inner_btn.classList.remove('btn-outline-danger');
-                            inner_btn.classList.add('disabled');
-                            inner_btn.classList.add('btn-outline-secondary');
-                            inner_btn.blur();
-                        }
-                    }
-                })
-                .catch(
-
-                );
-        })
+            moderationBtnClick(event, "approve");
+        });
+    for (let btn of postDeclineBtns)
+        btn.addEventListener("click", event => {
+            moderationBtnClick(event, "decline");
+        });
 })
