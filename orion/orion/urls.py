fc9b2eee6e0ee182c5dc9882123fc90f4b8a3dbd
AEpolyakov <ae.polyakov@mail.ru>
2022-03-22 23:37:15 +0400

    поиск через MainView. поиск по нескольким значениям.

--- orion/orion/urls.py
+++ orion/orion/urls.py
@@ -2,11 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
-from hub.views import MainView, SearchView
+from hub.views import MainView
 
 urlpatterns = [
     path('', MainView.as_view(), name='main'),
-    path('search/', SearchView.as_view(), name='search'),
     path('tinymce/', include('tinymce.urls')),
     path('hub/', include('hub.urls', namespace='hubs')),
     path('admin/', admin.site.urls),
